86f01bd90a09f9d0cf7507cc6e79e600efa04fd5
qiuxiaoliang <qiuxiaoliang@sisyphe.cn>
2019-02-22 15:45:44 +0800

    add selection sort

--- src/com/algorithm/Sort.java
+++ src/com/algorithm/Sort.java
@@ -15,11 +15,36 @@ public class Sort {
         for (int i = 0; i < array.length - 1; i++) {
             for (int j = i + 1; j < array.length; j++) {
                 if (array[i] > array[j]) {
-                    int tmp = array[i];
-                    array[i] = array[j];
-                    array[j] = tmp;
+                    swap(array, i, j);
                 }
             }
         }
     }
+
+    /**
+     * 选择排序
+     *
+     * @param array
+     */
+    public static void sortSelection(int[] array) {
+        int index = 0;
+        for (int i = 0; i < array.length - 1; i++) {
+            index = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[index] > array[j]) {
+                    index = j;
+                }
+            }
+            if (i != index) {
+                swap(array, i, index);
+            }
+        }
+    }
+
+    public static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
 }
